b3ab21aacee72c47b6c2c074bc37591bf3528b3d
eranho23-meet <109900208+eranho23-meet@users.noreply.github.com>
2022-07-31 19:08:24 +0300

    finished lab plus bonus
    because I am awsomeeeeeeeee
    yeyyyyyaaaaaaaa

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -26,23 +26,42 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 @app.route('/', methods=['GET', 'POST'])
 def signin():
     if request.method == 'POST':
-        mail = request.form('email')
-        word = request.form('password')
+        mail = request.form['email']
+        word = request.form['password']
         try:
-            login_session['user'] = auth.sign_in_with_email_and_password(mail, word)
-    else:
-        return render_template("signin.html")
+            login_session['user'] = auth.create_user_with_email_and_password(mail, word)
+            return redirect(url_for('add_tweet'))
+        except:
+            print('error bad')
+    return render_template("signin.html")
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-
+    if request.method == 'POST':
+        mail = request.form['email']
+        word = request.form['password']
+        try:
+            login_session['user'] = auth.sign_in_with_email_and_password(mail, word)
+            return redirect(url_for('add_tweet'))
+        except:
+            print('ERRORROROROROROROR bad')
     return render_template("signup.html")
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
+    if request.method == 'POST':
+        return redirect(url_for('signout'))
     return render_template("add_tweet.html")
+
+
+@app.route('/signout')
+def signout():
+    login_session['user'] = None
+    auth.current_user = None
+    return redirect(url_for('signin'))
+
 
 
 if __name__ == '__main__':
