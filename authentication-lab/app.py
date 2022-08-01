96388738d90295ad72584e074dc4d41f8b490c2d
eranho23-meet <109900208+eranho23-meet@users.noreply.github.com>
2022-08-01 15:08:04 +0300

    finished the lab
    we did it!
    we did it!
    yeah! we did it!

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -11,11 +11,12 @@ config = {
   "messagingSenderId": "285222370726",
   "appId": "1:285222370726:web:03d7cce950a51ff7de6367",
   "measurementId": "G-LPVLYQNTK9",
-  "databaseURL": ""
+  "databaseURL": "https://cool-project-d10a3-default-rtdb.europe-west1.firebasedatabase.app"
 }
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
+db = firebase.database()
 
 
 
@@ -24,20 +25,25 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 
 
 @app.route('/', methods=['GET', 'POST'])
-def signin():
+def signup():
     if request.method == 'POST':
         mail = request.form['email']
         word = request.form['password']
+        full_name = request.form['namename']
+        username = request.form['username']
+        bio = request.form['bio']
+        user = {'mail':mail, 'password':word, 'full_name':full_name, 'username':username, 'bio':bio}
         try:
             login_session['user'] = auth.create_user_with_email_and_password(mail, word)
+            db.child('Users').child(login_session['user']['localId']).set(user)
             return redirect(url_for('add_tweet'))
         except:
             print('error bad')
-    return render_template("signin.html")
+    return render_template("signup.html")
 
 
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
     if request.method == 'POST':
         mail = request.form['email']
         word = request.form['password']
@@ -46,14 +52,33 @@ def signup():
             return redirect(url_for('add_tweet'))
         except:
             print('ERRORROROROROROROR bad')
-    return render_template("signup.html")
+    return render_template("signin.html")
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
     if request.method == 'POST':
-        return redirect(url_for('signout'))
+        if request.args.get('f') == 'f1':
+            return redirect(url_for('signout'))
+        elif request.args.get('f') == 'f2':
+            return redirect(url_for('all_tweets'))
+        tweet = {'title':request.form['title'], 'content':request.form['con'], 'uid':login_session['user']['localId']}
+        try:
+           db.child('Articles').push(tweet)
+
+        except:
+            print('nononoonoononononono!')
+
     return render_template("add_tweet.html")
+
+
+@app.route('/all_tweets')
+def all_tweets():
+    return render_template('all_tweets.html', twitts = db.child('Articles').get().val())
+
+
+
+
 
 
 @app.route('/signout')
